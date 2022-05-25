970b21de010db55a3b0bb7e68a22a1fcbbca6980
Michael Ressler <36997670+mressler97@users.noreply.github.com>
2022-05-25 13:04:47 -0500

    first commit

---
+++ script.ps1
@@ -0,0 +1,26 @@
+﻿<#
+    Updated 1 Jan 2022
+    !!!Instructions!!!
+        - This uses relative path so it mustin the directory storing the incident folder
+        - Navigate to the folder inthe commaompt below or programitcally
+        - ENSURE all files are closed, could get error from trying to read/edit files that are open
+        - This is just a starting point, this function will pull the list of .txt files only
+#>
+cls
+
+# Returns ID names for all .txt files in '.\Incidents' folder as array of string
+function idNameList {
+
+    $fileList = Get-ChildItem ".\Incidents" -Filter *.txt  #-Filter limits the lines returned
+    $idList = @()
+
+    foreach ($incident in $fileList) {
+        $idList += $incident.Name   #removes the aditional data from the system.io file to provide a list of just .txt es
+    }
+    #$idList
+    return $idList
+}
+
+#Demo of what this function returns
+$fileList = idNameList
+$fileList
